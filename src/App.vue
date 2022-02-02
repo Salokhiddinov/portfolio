5945741ff48eabf4587f362e7ba8c54cfa506789
Nomonjon <nomonsalakhiddinov2002@gmail.com>
2022-02-02 21:28:57 +0500

    glitch bg added

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,31 @@
 <template>
-  
+<div class="app">
+  <BaseGlitch class="base-glitch"/>
+
+</div>
 </template>
 
+<script>
+import BaseGlitch from '@/components/base/BaseGlitch.vue';
+export default {
+  components: {BaseGlitch}
+}
+</script>
+
+<style>
+.app{
+  position: relative;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.base-glitch{
+  display: block;
+  position: absolute fixed;
+  z-index: 1;
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+</style>
